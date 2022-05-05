7e75b564f79f01595d48728f09c71546e879cb51
Abdulazeez403 <Abdulazeezsodiq403@gmail.com>
2022-05-05 00:44:36 -0700

    Updated file

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,7 @@
+const Contact = () => {
+ return (
+  <h4>I am the Contact Page</h4>
+   );
+}
+ 
+export default Contact;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+ return ( 
+  <h4>I am the Home page</h4>
+  );
+}
+ 
+export default Home;
